cd0b388427346f1d13d05f7b46c8ee904255cc6a
ldv31 <duval.ludovic@gmail.com>
2018-05-10 11:47:17 +0200

    Initial commit - for Github first test

---
+++ src/bataillenavale/BatailleNavaleManager.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bataillenavale;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LDV
+ */
+public class BatailleNavaleManager {
+    
+    // Boats parameters definition on the battlefield
+    private final int boatNumber = 3;
+    private final int boatSize = 3;
+    private final String boatName[] = {"Boat1", "boat2", "boat3"};
+    
+    // Battle field définiton
+    private final int xSize = 10;
+    private final int ySize = 10;
+    
+    // array of boats
+    private Boat gameBoat [] = new Boat [boatNumber];
+    
+    //in game variable
+    
+    // Exit game if yes
+    private boolean exitGame = false;
+    
+    // number of boat left (will end the game if set to 0)
+    private int numberOfBoatLeft = boatNumber;
+    
+    // number of trial from player (used for providing statistics to the player)
+    
+    // Run the game
+    public BatailleNavaleManager ()
+    {
+        // place boat on the battle field
+        initialiseBoat();
+    }
+    
+    private void initialiseBoat()
+    {
+        // set parameters values for all boats
+        for(int i=0; i<boatNumber; i++)
+        {
+            gameBoat[i] = new Boat (boatName[i],getPosition());
+            gameBoat[i].printPosition();
+        }
+    }
+    
+    // get a position for a boat on the battle field
+    private int[] getPosition ()
+    {
+        int position [] = new int [boatSize*2];
+        int maxLoop = 50;
+        boolean positionFound = false;
+        
+        // check if the position is already used, if yes, loop till a position is found (max 20 loop)
+        while (maxLoop > 0 && positionFound == false)
+        {
+           // calculate random position with the battlefield
+           for (int i=0; i < boatSize; i++)
+           {
+               Random rand = new Random();
+               position[i*2] = rand.nextInt(xSize);
+               position[i*2+1] = rand.nextInt(ySize);
+           }
+           positionFound = true;
+            
+            // for each existing boat, check is the new position intersects
+            for (int j = 0; j < boatNumber; j++)
+            {
+                if (gameBoat[j] != null)
+                {
+                    if (gameBoat[j].comparePosition(position,false) == true)
+                        positionFound = false;
+                }
+            }
+           maxLoop--;
+        }
+        
+        return position;
+    }
+    
+    public void runGame()
+    {
+        //scanner for input
+        Scanner playerScanner = new Scanner(System.in);
+        String playerInput;
+        
+        // ask player for hit coordinate and provide result, till all boats are sunk
+        while ((exitGame == false) && (numberOfBoatLeft > 0))
+        {
+            // Prepare input 
+            System.out.print("Entrez les coordonnées du tir: ");
+            playerInput = playerScanner.nextLine();
+           
+            // only two caracter for valid player coordinates entry
+            if (playerInput.length() != 2)
+            {
+                System.out.println("Nombre de caractères incorrect, essayez à nouveau (2 caractères)");
+            }
+            else
+            {
+                // convert caractère to coordinate
+                
+            }
+        }
+        
+        
+        /*    
+        Test section, to be commented for normal use
+        
+        int testBoat1Position [] = {0,0,1,1,2,2};
+        int testBoat2Position [] = {0,0,4,4,5,5};
+        Boat testBoat1 = new Boat("testBoat1", testBoat1Position);
+        // Verify tha creation of the boat objet is correct
+        testBoat1.printPosition();
+        // veify the comparePosition function
+        System.out.println("Intersect result: " + testBoat1.comparePosition(testBoat2Position, false));
+        */
+    }
+}
---
+++ src/bataillenavale/Boat.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bataillenavale;
+
+/**
+ *
+ * @author LDV
+ */
+
+// This classe define "boat" used in bataille navale
+public class Boat {
+    //boat parameters
+    private String name;
+
+    // position of the boat, passed to the boat object by the game manager object
+    private int position [];
+    
+    // hit point, set to 0 initialy
+    private int hitPoint = 0;
+    
+    // true if boat is sunk
+    boolean sinkStatus = false;
+    
+    // constructor
+    public Boat (String name, int position [])
+    {  
+        this.name = name;
+        this.position = new int[position.length];
+        
+        System.arraycopy(position, 0, this.position, 0, position.length);
+    }  
+    
+    // print the position of the boat (use to check that position is OK)
+    public void printPosition ()
+    {
+        System.out.println("Name: " + name);
+        System.out.print("Position: ");
+        for (int i=0; i < position.length; i++)
+        {
+            System.out.print(position [i] + " ");
+        }
+        System.out.println();
+    }
+    
+    // Compare too positions, if they intersect, return true
+    // use false for checking and true hor hit
+    public boolean comparePosition (int position [], boolean hit)
+    {
+        boolean intersection = false;
+        
+        for (int i = 0; i < position.length/2; i++)
+        {
+            if ((this.position [i*2] == position [i*2]) && (this.position [i*2+1] == position [i*2+1]))
+            {
+                intersection = true;
+            }
+        }
+        
+        if (intersection == true && hit == true)
+        {
+            hitPoint++;
+            if (hitPoint == position.length)
+                sinkStatus = true;
+        }
+        return intersection;
+    }
+    
+    
+    // get the postion
+    public int[] getPosition()
+    {
+        return position;
+    }
+    
+    // get sinkStatus
+    public boolean getSinkStatus()
+    {
+        return sinkStatus;
+    }
+}
